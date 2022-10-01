dd1e1d588499c69fb0ce8c4d3f0cd1029681407f
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2022-10-01 23:36:42 +0100

    Initial project Add

---
+++ emeka-pet-clinic/src/main/java/com/javastriker/emekapetclinic/EmekaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.javastriker.emekapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmekaPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmekaPetClinicApplication.class, args);
+    }
+
+}
---
+++ emeka-pet-clinic/src/test/java/com/javastriker/emekapetclinic/EmekaPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javastriker.emekapetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmekaPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
