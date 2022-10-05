77957c6444439aaeb26df31a42fecace48a2a97f
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2022-10-05 11:49:03 +0100

    implemented base entity. Closes #10

--- emeka-pet-clinic/pet-clinic-data/src/main/java/com/javastriker/emekapetclinic/model/Person.java
+++ emeka-pet-clinic/pet-clinic-data/src/main/java/com/javastriker/emekapetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.javastriker.emekapetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- emeka-pet-clinic/pet-clinic-data/src/main/java/com/javastriker/emekapetclinic/model/PetType.java
+++ emeka-pet-clinic/pet-clinic-data/src/main/java/com/javastriker/emekapetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.javastriker.emekapetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
