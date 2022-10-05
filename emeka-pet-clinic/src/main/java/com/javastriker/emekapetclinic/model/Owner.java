b40abfaad478838df7e8b7d6d7b6d0a69c92cd3a
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2022-10-05 04:03:54 +0100

    Added POJOs. Closes #2.

---
+++ emeka-pet-clinic/src/main/java/com/javastriker/emekapetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.javastriker.emekapetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ emeka-pet-clinic/src/main/java/com/javastriker/emekapetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.javastriker.emekapetclinic.model;
+
+public class Vet extends Person{
+}
